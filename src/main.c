917d166518d2b5d467ed3527d7fcf72a9f104699
Lucien Van Bussel <lvan-bus@f1r5s18.codam.nl>
2023-12-21 13:47:08 +0100

    moved some code to new function hooks

--- src/main.c
+++ src/main.c
@@ -97,162 +97,166 @@ void	screen(void *param)
 
 	cubed = param;
 	for(int x = 0; x < w; x++)
+	{
+		//calculate ray position and direction
+		double cameraX = 2 * x / (double)w - 1; //x-coordinate in camera space
+		double rayDirX = cubed->dirX + cubed->planeX * cameraX;
+		double rayDirY = cubed->dirY + cubed->planeY * cameraX;
+		//which box of the map we're in
+		int mapX = (int)cubed->posX;
+		int mapY = (int)cubed->posY;
+
+		//length of ray from current position to next x or y-side
+		double sideDistX;
+		double sideDistY;
+
+		//length of ray from one x or y-side to next x or y-side
+		//these are derived as:
+		//deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX))
+		//deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY))
+		//which can be simplified to abs(|rayDir| / rayDirX) and abs(|rayDir| / rayDirY)
+		//where |rayDir| is the length of the vector (rayDirX, rayDirY). Its length,
+		//unlike (dirX, dirY) is not 1, however this does not matter, only the
+		//ratio between deltaDistX and deltaDistY matters, due to the way the DDA
+		//stepping further below works. So the values can be computed as below.
+		// Division through zero is prevented, even though technically that's not
+		// needed in C++ with IEEE 754 floating point values.
+		double deltaDistX = (rayDirX == 0) ? 1e30 : abs(1 / rayDirX);
+		double deltaDistY = (rayDirY == 0) ? 1e30 : abs(1 / rayDirY);
+
+		double perpWallDist;
+
+		//what direction to step in x or y-direction (either +1 or -1)
+		int stepX;
+		int stepY;
+
+		int hit = 0; //was there a wall hit?
+		int side; //was a NS or a EW wall hit?
+		//calculate step and initial sideDist
+		if(rayDirX < 0)
 		{
-			//calculate ray position and direction
-			double cameraX = 2 * x / (double)w - 1; //x-coordinate in camera space
-			double rayDirX = dirX + planeX * cameraX;
-			double rayDirY = dirY + planeY * cameraX;
-			//which box of the map we're in
-			int mapX = int(posX);
-			int mapY = int(posY);
-
-			//length of ray from current position to next x or y-side
-			double sideDistX;
-			double sideDistY;
-
-			//length of ray from one x or y-side to next x or y-side
-			//these are derived as:
-			//deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX))
-			//deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY))
-			//which can be simplified to abs(|rayDir| / rayDirX) and abs(|rayDir| / rayDirY)
-			//where |rayDir| is the length of the vector (rayDirX, rayDirY). Its length,
-			//unlike (dirX, dirY) is not 1, however this does not matter, only the
-			//ratio between deltaDistX and deltaDistY matters, due to the way the DDA
-			//stepping further below works. So the values can be computed as below.
-			// Division through zero is prevented, even though technically that's not
-			// needed in C++ with IEEE 754 floating point values.
-			double deltaDistX = (rayDirX == 0) ? 1e30 : abs(1 / rayDirX);
-			double deltaDistY = (rayDirY == 0) ? 1e30 : abs(1 / rayDirY);
-
-			double perpWallDist;
-
-			//what direction to step in x or y-direction (either +1 or -1)
-			int stepX;
-			int stepY;
-
-			int hit = 0; //was there a wall hit?
-			int side; //was a NS or a EW wall hit?
-			//calculate step and initial sideDist
-			if(rayDirX < 0)
+			stepX = -1;
+			sideDistX = (cubed->posX - mapX) * deltaDistX;
+		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - cubed->posX) * deltaDistX;
+		}
+		if(rayDirY < 0)
+		{
+			stepY = -1;
+			sideDistY = (cubed->posY - mapY) * deltaDistY;
+		}
+		else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - cubed->posY) * deltaDistY;
+		}
+		//perform DDA
+		while(hit == 0)
+		{
+		//jump to next map square, either in x-direction, or in y-direction
+			if(sideDistX < sideDistY)
 			{
-				stepX = -1;
-				sideDistX = (posX - mapX) * deltaDistX;
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
 			}
 			else
 			{
-				stepX = 1;
-				sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
 			}
-			if(rayDirY < 0)
-			{
-				stepY = -1;
-				sideDistY = (posY - mapY) * deltaDistY;
-			}
-			else
-			{
-				stepY = 1;
-				sideDistY = (mapY + 1.0 - posY) * deltaDistY;
-			}
-			//perform DDA
-			while(hit == 0)
-			{
-			//jump to next map square, either in x-direction, or in y-direction
-				if(sideDistX < sideDistY)
-				{
-					sideDistX += deltaDistX;
-					mapX += stepX;
-					side = 0;
-				}
-				else
-				{
-					sideDistY += deltaDistY;
-					mapY += stepY;
-					side = 1;
-				}
-				//Check if ray has hit a wall
-				if(worldMap[mapX][mapY] > 0) hit = 1;
-			}
-			//Calculate distance projected on camera direction. This is the shortest distance from the point where the wall is
-			//hit to the camera plane. Euclidean to center camera point would give fisheye effect!
-			//This can be computed as (mapX - posX + (1 - stepX) / 2) / rayDirX for side == 0, or same formula with Y
-			//for size == 1, but can be simplified to the code below thanks to how sideDist and deltaDist are computed:
-			//because they were left scaled to |rayDir|. sideDist is the entire length of the ray above after the multiple
-			//steps, but we subtract deltaDist once because one step more into the wall was taken above.
-			if(side == 0) perpWallDist = (sideDistX - deltaDistX);
-			else          perpWallDist = (sideDistY - deltaDistY);
-
-			//Calculate height of line to draw on screen
-			int lineHeight = (int)(h / perpWallDist);
-
-			//calculate lowest and highest pixel to fill in current stripe
-			int drawStart = -lineHeight / 2 + h / 2;
-			if(drawStart < 0) drawStart = 0;
-			int drawEnd = lineHeight / 2 + h / 2;
-			if(drawEnd >= h) drawEnd = h - 1;
-
-			//choose wall color
-			ColorRGB color;
-			switch(worldMap[mapX][mapY])
-			{
-				case 1:  color = RGB_Red;    break; //red
-				case 2:  color = RGB_Green;  break; //green
-				case 3:  color = RGB_Blue;   break; //blue
-				case 4:  color = RGB_White;  break; //white
-				default: color = RGB_Yellow; break; //yellow
-			}
-
-			//give x and y sides different brightness
-			if(side == 1) {color = color / 2;}
-
-			//draw the pixels of the stripe as a vertical line
-			verLine(x, drawStart, drawEnd, color); // replace for mlx_put_pixel
+			//Check if ray has hit a wall
+			if(worldMap[mapX][mapY] > 0) hit = 1;
 		}
-		//timing for input and FPS counter
-		oldTime = time;
-		time = getTicks();
-		double frameTime = (time - oldTime) / 1000.0; //frameTime is the time this frame has taken, in seconds
-		print(1.0 / frameTime); //FPS counter
-		redraw();
-		cls();
+		//Calculate distance projected on camera direction. This is the shortest distance from the point where the wall is
+		//hit to the camera plane. Euclidean to center camera point would give fisheye effect!
+		//This can be computed as (mapX - posX + (1 - stepX) / 2) / rayDirX for side == 0, or same formula with Y
+		//for size == 1, but can be simplified to the code below thanks to how sideDist and deltaDist are computed:
+		//because they were left scaled to |rayDir|. sideDist is the entire length of the ray above after the multiple
+		//steps, but we subtract deltaDist once because one step more into the wall was taken above.
+		if(side == 0) perpWallDist = (sideDistX - deltaDistX);
+		else          perpWallDist = (sideDistY - deltaDistY);
 
-		//speed modifiers
-		double moveSpeed = frameTime * 5.0; //the constant value is in squares/second
-		double rotSpeed = frameTime * 3.0; //the constant value is in radians/second
-		readKeys();
-		//move forward if no wall in front of you
-		if(keyDown(SDLK_UP))
+		//Calculate height of line to draw on screen
+		int lineHeight = (int)(h / perpWallDist);
+
+		//calculate lowest and highest pixel to fill in current stripe
+		int drawStart = -lineHeight / 2 + h / 2;
+		if(drawStart < 0) drawStart = 0;
+		int drawEnd = lineHeight / 2 + h / 2;
+		if(drawEnd >= h) drawEnd = h - 1;
+
+		//choose wall color
+		ColorRGB color;
+		switch(worldMap[mapX][mapY])
 		{
-			if(worldMap[int(posX + dirX * moveSpeed)][int(posY)] == false) posX += dirX * moveSpeed;
-			if(worldMap[int(posX)][int(posY + dirY * moveSpeed)] == false) posY += dirY * moveSpeed;
+			case 1:  color = RGB_Red;    break; //red
+			case 2:  color = RGB_Green;  break; //green
+			case 3:  color = RGB_Blue;   break; //blue
+			case 4:  color = RGB_White;  break; //white
+			default: color = RGB_Yellow; break; //yellow
 		}
-		//move backwards if no wall behind you
-		if(keyDown(SDLK_DOWN))
-		{
-			if(worldMap[int(posX - dirX * moveSpeed)][int(posY)] == false) posX -= dirX * moveSpeed;
-			if(worldMap[int(posX)][int(posY - dirY * moveSpeed)] == false) posY -= dirY * moveSpeed;
-		}
-		//rotate to the right
-		if(keyDown(SDLK_RIGHT))
-		{
-			//both camera direction and camera plane must be rotated
-			double oldDirX = dirX;
-			dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
-			dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
-			double oldPlaneX = planeX;
-			planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
-			planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
-		}
-		//rotate to the left
-		if(keyDown(SDLK_LEFT))
-		{
-			//both camera direction and camera plane must be rotated
-			double oldDirX = dirX;
-			dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
-			dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
-			double oldPlaneX = planeX;
-			planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
-			planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
-		}
+
+		//give x and y sides different brightness
+		if(side == 1) {color = color / 2;}
+
+		//draw the pixels of the stripe as a vertical line
+		verLine(x, drawStart, drawEnd, color); // replace for mlx_put_pixel
+	}
+}
+
+void	hooks(t_cubed *cubed)
+{
+	//timing for input and FPS counter
+	cubed->oldTime = cubed->time;
+	cubed->time = getTicks();
+	double frameTime = (cubed->time - cubed->oldTime) / 1000.0; //frameTime is the time this frame has taken, in seconds
+	print(1.0 / frameTime); //FPS counter
+	redraw();
+	cls();
+
+	//speed modifiers
+	double moveSpeed = frameTime * 5.0; //the constant value is in squares/second
+	double rotSpeed = frameTime * 3.0; //the constant value is in radians/second
+	readKeys();
+	//move forward if no wall in front of you
+	if(mlx_is_key_down(mlx, MLX_KEY_W))
+	{
+		if(worldMap[(int)(cubed->posX + cubed->dirX * moveSpeed)][(int)(cubed->posY)] == false) cubed->posX += cubed->dirX * moveSpeed;
+		if(worldMap[(int)(cubed->posX)][(int)(cubed->posY + cubed->dirY * moveSpeed)] == false) cubed->posY += cubed->dirY * moveSpeed;
+	}
+	//move backwards if no wall behind you
+	if(mlx_is_key_down(mlx, MLX_KEY_S))
+	{
+		if(worldMap[(int)(cubed->posX - cubed->dirX * moveSpeed)][(int)(cubed->posY)] == false) cubed->posX -= cubed->dirX * moveSpeed;
+		if(worldMap[(int)(cubed->posX)][(int)(cubed->posY - cubed->dirY * moveSpeed)] == false) cubed->posY -= cubed->dirY * moveSpeed;
+	}
+	//rotate to the right
+	if(mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+	{
+		//both camera direction and camera plane must be rotated
+		double oldDirX = cubed->dirX;
+		cubed->dirX = cubed->dirX * cos(-rotSpeed) - cubed->dirY * sin(-rotSpeed);
+		cubed->dirY = oldDirX * sin(-rotSpeed) + cubed->dirY * cos(-rotSpeed);
+		double oldPlaneX = cubed->planeX;
+		cubed->planeX = cubed->planeX * cos(-rotSpeed) - cubed->planeY * sin(-rotSpeed);
+		cubed->planeY = oldPlaneX * sin(-rotSpeed) + cubed->planeY * cos(-rotSpeed);
+	}
+	//rotate to the left
+	if(mlx_is_key_down(mlx, MLX_KEY_LEFT))
+	{
+		//both camera direction and camera plane must be rotated
+		double oldDirX = cubed->dirX;
+		cubed->dirX = cubed->dirX * cos(rotSpeed) - cubed->dirY * sin(rotSpeed);
+		cubed->dirY = oldDirX * sin(rotSpeed) + cubed->dirY * cos(rotSpeed);
+		double oldPlaneX = cubed->planeX;
+		cubed->planeX = cubed->planeX * cos(rotSpeed) - cubed->planeY * sin(rotSpeed);
+		cubed->planeY = oldPlaneX * sin(rotSpeed) + cubed->planeY * cos(rotSpeed);
+	}
 }
 
 void	initialize_cubed(t_cubed *cubed)
@@ -290,7 +294,7 @@ int	mlx(t_cubed *cubed)
 	}
 
 	mlx_loop_hook(cubed->mlx, screen, cubed);
-	// mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop_hook(cubed->mlx, hooks, cubed);
 
 	mlx_loop(cubed->mlx);
 	mlx_terminate(cubed->mlx);
