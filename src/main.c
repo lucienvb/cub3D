44f6e6d30b94dd3561eb34716eac47db6dd5dcba
Lucien Van Bussel <lvan-bus@f1r6s8.codam.nl>
2024-03-21 12:52:13 +0100

    fixed signal error

--- src/main.c
+++ src/main.c
@@ -1,21 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: cter-maa <cter-maa@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2024/03/18 09:56:04 by chavertterm   #+#    #+#                 */
-/*   Updated: 2024/03/19 15:15:52 by cter-maa      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvan-bus <lvan-bus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 09:56:04 by chavertterm       #+#    #+#             */
+/*   Updated: 2024/03/21 12:51:05 by lvan-bus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cubed.h"
+#include <signal.h>
 
 int	main(int argc, char **argv)
 {
 	t_cubed		cubed;
 
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 	input_error_handling(argc, argv);
 	init_setup(&cubed, argv[1]);
 	if (input_parsing(&cubed, argv[1]) == FAILURE)
